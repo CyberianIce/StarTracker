4ba29d287c8524eb4868f32e284dd9eea5e2adc0
CyberianIce <cyberianice@gmail.com>
2021-01-09 19:33:42 +0100

    Field Tests

--- src/StarTracker/StarTracker.ino
+++ src/StarTracker/StarTracker.ino
@@ -1,30 +1,42 @@
 #include <AccelStepper.h>
+#include <EEPROM.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define tonePin 2
-#define rewindPin 9
-#define forwardPin 10
 #define stepPin 11
 #define dirPin 12
 
 String firmware = "1.0.0";
 short mode = 0;
 float raSiderealRate = 6.610641;
+float trackSpeed = raSiderealRate;
 float fastSpeed = 2000;
+String command = "";
+float newRate = raSiderealRate;
 AccelStepper RA = AccelStepper(1, stepPin, dirPin);
 Adafruit_SSD1306 OLED = Adafruit_SSD1306(128, 64, &Wire);
 
 void setup() {
+  trackSpeed = EEPROM.get(0, trackSpeed);
+  if(trackSpeed == 'ovf' || trackSpeed == 'nan') {
+    trackSpeed = EEPROM.put(0, raSiderealRate);
+    trackSpeed = raSiderealRate;
+  }
+  Serial.begin(57600);
+  Serial.setTimeout(10);
+  Serial.print("Tracking rate: ");
+  Serial.print(trackSpeed, 6);
+  Serial.println(" Hz");
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-  pinMode(forwardPin, INPUT_PULLUP);
-  pinMode(rewindPin, INPUT_PULLUP);
   RA.setPinsInverted(false);
   RA.setMaxSpeed(8000);
-  RA.setSpeed(raSiderealRate);
+  RA.setSpeed(trackSpeed);
+  RA.setPinsInverted(true);
+  RA.setAcceleration(2000);
   OLED.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   OLED.setTextColor(SSD1306_WHITE);
   OLED.clearDisplay();
@@ -34,20 +46,43 @@ void setup() {
   OLED.display();
   OLED.clearDisplay();
   OLED.setCursor(0, 0);
-  introMellody();
+  // introMellody();
 }
 
 void loop() {
+  
+  if(Serial.available() > 0) {
+    mode = 0;
+    command = Serial.readStringUntil("\n");
+    newRate = command.toFloat();
+    if(newRate > 0 && newRate <=2000) {
+      trackSpeed = newRate;
+      EEPROM.put(0, trackSpeed);
+      beep(698, 50);
+      Serial.print("Tracking rate: ");
+      Serial.print(trackSpeed, 6);
+      Serial.println(" Hz");
+    } else if(command == "RST#") {
+      EEPROM.put(0, raSiderealRate);
+      trackSpeed = raSiderealRate;
+      beep(440, 100);
+      beep(698, 120);
+      beep(1000, 140);
+      Serial.print("Tracking rate: ");
+      Serial.print(trackSpeed, 6);
+      Serial.println(" Hz");
+    } else if(command == "+#") {
+      RA.setCurrentPosition(0);
+      RA.setSpeed(1000);
+      RA.runToNewPosition(1582);
+    } else if(command == "-#") {
+      RA.setCurrentPosition(0);
+      RA.setSpeed(1000);
+      RA.runToNewPosition(-1582);
+    }
+  }
 
-  uint8_t rewindPin_mask = digitalPinToBitMask(rewindPin);
-  volatile uint8_t *rewindPin_port = portInputRegister(digitalPinToPort(rewindPin));
-  uint8_t rewindPinState = (*rewindPin_port & rewindPin_mask) != 0;
-
-  uint8_t forwardPin_mask = digitalPinToBitMask(forwardPin);
-  volatile uint8_t *forwardPin_port = portInputRegister(digitalPinToPort(forwardPin));
-  uint8_t forwardPinState = (*forwardPin_port & forwardPin_mask) != 0;
- 
-  if(!rewindPinState) {
+  if(command == "RWD#") {
      if (mode != 2) {
       OLED.clearDisplay();
       OLED.setCursor(40, 6);
@@ -58,11 +93,14 @@ void loop() {
       OLED.setTextSize(1);
       OLED.println("REWIND");
       OLED.display();
-      RA.setPinsInverted(true);
+      RA.setPinsInverted(false);
       RA.setSpeed(fastSpeed);
+      Serial.print("Rewinding rate: ");
+      Serial.print(fastSpeed);
+      Serial.println(" Hz");
     }
     mode = 2;
-  } else if(!forwardPinState) {
+  } else if (command == "FWD#") {
     if (mode != 3) {
       OLED.clearDisplay();
       OLED.setCursor(40, 6);
@@ -73,24 +111,29 @@ void loop() {
       OLED.setTextSize(1);
       OLED.println("FORWARD");
       OLED.display();
-      RA.setPinsInverted(false);
+      RA.setPinsInverted(true);
       RA.setSpeed(fastSpeed);
+      Serial.print("Forwarding rate: ");
+      Serial.print(fastSpeed);
+      Serial.println(" Hz");
     }
     mode = 3;
   } else {
     if(mode != 1) {
       OLED.clearDisplay();
-      OLED.setCursor(58, 6);
+      OLED.setCursor(58, 0);
       OLED.setTextSize(3);
       OLED.println((char)16);
-      OLED.setCursor(42, 32);
+      OLED.setCursor(42, 24);
       OLED.setTextSize(1);
       OLED.println("SIDEREAL");
-      OLED.setCursor(42, 42);
+      OLED.setCursor(42, 34);
       OLED.println("TRACKING");
+      OLED.setCursor(42, 44);
+      OLED.println(trackSpeed, 6);
       OLED.display();
-      RA.setPinsInverted(false);
-      RA.setSpeed(raSiderealRate);
+      RA.setPinsInverted(true);
+      RA.setSpeed(trackSpeed);
     }
     mode = 1;
   }
